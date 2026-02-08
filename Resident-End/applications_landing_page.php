b3445109e4c9a43294fe8aa6e888250b1a2306fa
JewellaMarie <maranion.jewellamarie@ue.edu.ph>
2026-02-08 16:23:50 +0800

    Rename certificate landing page to applications landing page

--- Resident-End/applications_landing_page.php
+++ Resident-End/applications_landing_page.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Certificate Application</title>
+    <title>Document Application</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../CSS-Styles/GeneralStyle.css">
@@ -102,5 +102,6 @@
 
 
            
+
 
 
