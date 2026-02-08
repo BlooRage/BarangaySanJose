7a636659605eb389ccc3731558aa1098da7fae10
JewellaMarie <maranion.jewellamarie@ue.edu.ph>
2026-02-08 23:36:01 +0800

    brgy id cert landing

--- Resident-End/ApplicationsLandingPage.php
+++ Resident-End/ApplicationsLandingPage.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../CSS-Styles/GeneralStyle.css">
-    <link rel="stylesheet" href="../CSS-Styles/Resident-End-CSS/CertificateLandingPage.css">
+    <link rel="stylesheet" href="../CSS-Styles/Resident-End-CSS/ApplicationLandingPage.css">
 </head>
 
 <body>
