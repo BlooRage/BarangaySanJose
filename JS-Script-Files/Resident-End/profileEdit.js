9b116d39f3c37c687e8a73e0b49843cf9190acc5
BlooRage <mendozajerome050704@gmail.com>
2026-02-17 16:24:16 +0800

    most of the things updated

--- JS-Script-Files/Resident-End/profileEdit.js
+++ JS-Script-Files/Resident-End/profileEdit.js
@@ -315,6 +315,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
     const confirmDocumentRequirement = () =>
         new Promise((resolve) => {
             if (window.UniversalModal?.open) {
@@ -339,6 +340,8 @@ document.addEventListener("DOMContentLoaded", () => {
             resolve(window.confirm("Every applied change request requires supporting documents for verification. Continue?"));
         });
 
+=======
+>>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
 =======
