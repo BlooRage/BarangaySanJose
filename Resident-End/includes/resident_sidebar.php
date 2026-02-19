dc332f574db90e8a783f4b521d8debd6307246d7
JewellaMarie <maranion.jewellamarie@ue.edu.ph>
2026-02-19 13:13:06 +0800

    foldeer

--- Resident-End/includes/resident_sidebar.php
+++ Resident-End/includes/resident_sidebar.php
@@ -187,16 +187,11 @@ if ($residentId !== '' && isset($conn) && $conn instanceof mysqli) {
 
       <div id="group-navServices" class="mb-3">
         <p class="text-muted small fw-bold mb-1">Services</p>
-        <!-- add another for ApplicationsLandingPage.php -->
         <a href="ApplicationsLandingPage.php"
-           class="a-sidebarLink <?= activeLink('ApplicationsLandingPage.php', $current) ?>">
-          <i class="fa-solid fa-file-pen"></i>Document Application
-        </a>
-        <a href="resident_certificates.php"
            class="a-sidebarLink <?= activeLink('resident_certificates.php', $current) ?>">
           <i class="fa-solid fa-certificate"></i>Certificates
         </a>
-        <a href="resident_clearances.php"
+        <a href="Clearances.php"
            class="a-sidebarLink <?= activeLink('resident_clearances.php', $current) ?>">
           <i class="fa-solid fa-file-circle-check fa-sm"></i>Clearances
         </a>
