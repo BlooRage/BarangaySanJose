c92721d32f02a6a56ea832c8791dc7d0c1ffd5a3
BlooRage <161796330+BlooRage@users.noreply.github.com>
2026-07-21 04:17:07 +0800

    yes

--- Admin-End/Certificates/CertificateTracker.php
+++ Admin-End/Certificates/CertificateTracker.php
@@ -4254,6 +4254,6 @@ if ($certificateLaunchStage === 'release') {
 <script src="../../JS-Script-Files/Resident-End/dateFieldModal.js?v=20260707-date-proxy-white"></script>
 <script src="../../JS-Script-Files/Admin-End/tableColumnsGeneric.js?v=20260215-1"></script>
 <script src="../../JS-Script-Files/Shared/barangayIdDigital.js?v=20260718-address-dedupe-33"></script>
-<script src="../../JS-Script-Files/Admin-End/certificateTrackerScript.js?v=20260720-submitted-pending-pill"></script>
+<script src="../../JS-Script-Files/Admin-End/certificateTrackerScript.js?v=20260720-general-certification-2"></script>
 </body>
 </html>
